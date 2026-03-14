eaba65eec55044f39b93d30c627f4378dce5d266
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-14 12:55:13 +0200

    Finish intial set up for WebSocket server

---
+++ application-layer/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+const port = process.env.PORT || 3000;
+
