657e6b93304ac38fd8c9a96f7ee6d80f488783a0
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-04-25 14:11:40 +0300

    Added initial implementation for error handling

---
+++ application-layer/src/middleware/errorHandler.ts
@@ -0,0 +1,7 @@
+import { type ErrorMiddlware } from "../types/express/functions";
+
+const handleError: ErrorMiddlware = async (err, req, res, next) => {
+
+}
+
+export default handleError;
--- application-layer/src/types/express/functions.ts
+++ application-layer/src/types/express/functions.ts
@@ -2,8 +2,10 @@ import { NextFunction, Request, Response } from "express";
 
 type DefaultController = (req: Request, res: Response) => Promise<any>;
 type DefaultMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+type ErrorMiddlware = (err, req: Request, res: Response, next: NextFunction) => Promise<any>;
 
 export {
   DefaultController,
   DefaultMiddleware,
+  ErrorMiddlware,
 }
