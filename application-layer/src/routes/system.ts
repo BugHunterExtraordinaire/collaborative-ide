40f680c77d82f223b2dd0f046f66c5c8f8def62d
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-05-02 05:57:49 +0300

    Implement admin fetch sessions route and controllers

--- application-layer/src/routes/system.ts
+++ application-layer/src/routes/system.ts
@@ -5,12 +5,16 @@ import { authenticateUser } from '../middleware';
 import {
   getContainers,
   deleteContainer,
+  getSessions,
 } from '../controllers/system';
 
 const router = Router();
 
-router.get('/containers', authenticateUser, getContainers);
+router.use(authenticateUser);
 
-router.delete('/containers/:id', authenticateUser, deleteContainer);
+router.get('/containers', getContainers);
+router.get('/sessions', getSessions);
+
+router.delete('/containers/:id', deleteContainer);
 
 export default router;
