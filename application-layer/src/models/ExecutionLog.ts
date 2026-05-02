a846573337af51a3198fc3cd1d64fac73874f698
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-05-02 07:56:08 +0300

    Implement execution log models

---
+++ application-layer/src/models/ExecutionLog.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from 'mongoose';
+import { IExecutionLog } from '../types/mongoose/interfaces';
+
+const ExecutionLogSchema = new Schema({
+  session_id: { 
+    type: String, 
+    required: true 
+  },
+  username: { 
+    type: String, 
+    required: true 
+  },
+  input: { 
+    type: String, 
+    required: true 
+  },
+  output: { 
+    type: String, 
+    required: true 
+  },
+  status: { 
+    type: String, 
+    enum: ['Success', 'Error', 'Timeout'], 
+    required: true 
+  },
+  duration_ms: { 
+    type: Number, 
+    required: true 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+export default mongoose.model<IExecutionLog>('ExecutionLog', ExecutionLogSchema);
