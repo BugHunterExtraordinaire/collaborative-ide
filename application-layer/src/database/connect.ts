2fbc1a3e185900eb45369892cd9144fecdf02df8
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-05-11 02:54:18 +0300

    Finalize wsl integration

--- application-layer/src/database/connect.ts
+++ application-layer/src/database/connect.ts
@@ -5,6 +5,7 @@ import { MongooseConnectFunction } from "../types/mongoose/functions";
 const connectDB: MongooseConnectFunction = async (MONGO_URI) => {
   return await mongoose.connect(MONGO_URI, {
     dbName: "COLLABORATIVE_CODING_SITE",
+    family: 4,
   });
 }
 
